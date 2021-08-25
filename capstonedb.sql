c6419ef44209184417cb29af3f944e48862a2d2d
Raluca Lupu <radu_raluca76@yahoo.com>
2021-08-25 13:32:07 +0100

    Update capstonedb.sql

--- capstonedb.sql
+++ capstonedb.sql
@@ -35,10 +35,30 @@ roleId int not null,
 Foreign Key fk_posts_role(roleId)
 references `role`(`id`),
 
-imageId int not null,
+imageId int,
 
 Foreign Key fk_posts_images(imageId)
 references images(`id`),
 
 post varchar(1000) not null
 );
+
+insert into `role`(`id`,`role`)
+    values(1,"ADMIN"), (2,"TOP_USER"), (3,"USER");
+
+insert into `user`(`id`,`username`,`password`,`enabled`, roleId)
+    values(101,"admin", "admin01", true, 1),
+        (102,"topUser","topuser",true, 2),
+        (103,"normalUser","user",false, 3);
+
+insert into images(`id`, imageURL)
+    values(301,"url1"), (302,"url2"), (303,"url3");
+        
+insert into posts(postId, userId, roleId, imageId, post)
+values
+('401','101','1','303','post1'),
+('402','101','1','302','post2'),
+('403','102','2','302','post3'),
+('404','102','2','301','post4'),
+('405','102','2','301','post5'),
+('406','102','2','303','post6');
