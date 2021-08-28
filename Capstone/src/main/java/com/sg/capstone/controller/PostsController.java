b2c304013db8fe3d6a45662c7331e657bf44f2b9
PhillipeManta <filip.manta@yahoo.com>
2021-08-28 13:33:17 +0300

    commited

--- Capstone/src/main/java/com/sg/capstone/controller/PostsController.java
+++ Capstone/src/main/java/com/sg/capstone/controller/PostsController.java
@@ -6,14 +6,19 @@
 package com.sg.capstone.controller;
 
 import com.sg.capstone.models.Content;
+import com.sg.capstone.models.Posts;
 import com.sg.capstone.service.PostsService;
 import com.sg.capstone.service.PostsServiceImpl;
 import com.sg.capstone.service.StaticPageService;
 import com.sg.capstone.service.StaticPageServiceImpl;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  *
@@ -21,6 +26,16 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class PostsController {
+
+    @Autowired
+    PostsService postsService;
+
+    @GetMapping("BlogPosts")
+    public String displayPosts(Model model) {
+        List<Posts> teachers = postsService.getAllPosts();
+        model.addAttribute("teachers", teachers);
+        return "teachers";
+    }
 
 
 }
