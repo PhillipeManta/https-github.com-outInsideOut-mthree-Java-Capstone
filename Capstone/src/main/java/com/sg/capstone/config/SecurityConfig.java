be21c9b5b58930aaa4bfd48db1fae0eefd32bdb2
psyig <psyig@exmail.nottingham.ac.uk>
2021-08-28 16:30:37 +0100

    changes

--- Capstone/src/main/java/com/sg/capstone/config/SecurityConfig.java
+++ Capstone/src/main/java/com/sg/capstone/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/addBlog").hasRole("USER")
-                    .antMatchers("/", "/index", "/staticPage").permitAll()
+                    .antMatchers("/addBlog.html").hasRole("USER")
+                    .antMatchers("/", "/index.html", "/staticPage.html").permitAll()
                 .and()
                 .formLogin()
                     .loginPage("/logon.html")
